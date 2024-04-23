96553000224f5f795b3d83c26c101a2d3f51175f
guilherme <guirossik10@gmail.com>
2024-04-23 20:16:38 -0300

    O melhor commit

--- assets/JS/cadastrar.js
+++ assets/JS/cadastrar.js
@@ -1,34 +1,39 @@
 document.querySelector("#botao-cadastrar").addEventListener("click", () => {
+  const form = document.querySelector("form");
 
-    const form = document.querySelector("form")
+  const tarefa = {
+    titulo: form.select_title.value,
+    mode: form.select_mode.value,
+    descricao: form.descricao.value,
+    pontos: form.pontos.value,
+  };
 
-    const tarefa = {
-        
-        titulo: form.titulo.value,
-        descricao: form.descricao.value,
-        pontos: form.pontos.value
-    }
+  validar(tarefa);
 
-    validar(tarefa)
-    console.log(tarefa)
-})
+  console.log(tarefa);
+});
 
-function validar(tarefa){
-    limparErros()
-    if (tarefa.titulo.trim() == ""){
-        document.querySelector("#titulo").classList.add("is-error")
-        document.querySelector("#titulo-erro").innerText = "O Título é obrigatório"
-    }
-    if (tarefa.descricao.trim() == "" || tarefa.descricao.length < 10){
-        document.querySelector("#descricao").classList.add("is-error")
-        document.querySelector("#descricao-erro").innerText = "A Descrição deve ter pelo menos 10 caracteres"
-    }
-    if (tarefa.pontos <= 0){
-        document.querySelector("#pontos").classList.add("is-error")
-        document.querySelector("#pontos-erro").innerText = "Os pontos devem ser maior que zero"
-    }
+function validar(tarefa) {
+  //   limparErros();
+  if (tarefa.titulo.trim() == "") {
+    document.querySelector("#error_Hb").classList.add("is-error");
+    document.querySelector("#select_title_erro").innerText =
+      "A Seleção da Habilidade é Obrigatório";
+  }
+
+  if (tarefa.descricao.trim() == "" || tarefa.descricao.length < 10) {
+    document.querySelector("#descricao").classList.add("is-error");
+    document.querySelector("#descricao-erro").innerText =
+      "A Descrição deve ter pelo menos 10 caracteres";
+  }
+  if (tarefa.pontos <= 0) {
+    document.querySelector("#pontos").classList.add("is-error");
+    document.querySelector("#pontos-erro").innerText =
+      "Os pontos devem ser maior que zero";
+  }
 }
-
-function limparErros(){
-    document.querySelectorAll("input .is-error, textarea .iserror").classList.remove("is-error")
-}
+// function limparErros() {
+//   document
+//     .querySelectorAll("input .is-error, textarea .iserror")
+//     .classList.remove("is-error");
+// }
