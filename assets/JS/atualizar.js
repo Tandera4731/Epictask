f9957f40417ae31bdc8d720bff19d7e9db9ddc1e
Tandera4731 <victoriopereira30@gmail.com>
2024-05-08 09:18:31 -0300

    Apagar e Atualizar div

---
+++ assets/JS/atualizar.js
@@ -0,0 +1,17 @@
+function inc(id){
+    const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+    let tarefa = tarefas.find(tarefa => tarefa.id = id)
+    tarefas.status += 10
+
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+    document.querySelector("#" + id + " progress").value += 10
+}
+
+function dec(id){
+    const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+    let tarefa = tarefas.find(tarefa => tarefa.id = id)
+    tarefas.status -= 10
+
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+    document.querySelector("#" + id + " progress").value -= 10
+}
